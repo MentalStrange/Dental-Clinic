63b2c4f71431b3ab3457c03fc236a6fc01c9cef3
mentalstrange <mohamed.ramadan2393@gmail.com>
2023-11-26 22:03:56 +0200

    little bit change in extenstions

--- BackEnd/routes/review.js
+++ BackEnd/routes/review.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { getAllReviews, createReview } from "../controllers/reviewController";
-import { authenticate, restrict } from "../auth/verfiyToken";
+import {
+  getAllReviews,
+  createReview,
+} from "../controllers/reviewController.js";
+import { authenticate, restrict } from "../auth/verfiyToken.js";
 
 const router = express.Router();
 
